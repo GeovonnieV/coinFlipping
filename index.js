8bc8c58424b0ed82c983cb155d80eebc330203f1
geovonnie vasquez <geovonnievasquez@gmail.com>
2020-12-22 14:26:08 -0600

    promis furfilled

---
+++ index.js
@@ -0,0 +1,51 @@
+// function fiveHeadsSync() {
+//         let headsCount = 0;
+//         let attempts = 0;
+//         // while headsCount is less than 5
+//         while(headsCount < 5) {
+//             // keep increasing attempts by one
+//             attempts++;
+//             // result is set to what the toss outcome was
+//             let result = tossCoin();
+//             console.log(`${result} was flipped`);
+//             if(result === "heads") {
+//                 headsCount++;
+//             } else {
+//                 headsCount = 0;
+//             }
+//         }
+//         return `It took ${attempts} tries to flip five "heads"`;
+//     }
+//     console.log( fiveHeadsSync() );
+//     console.log( "This is run after the fiveHeadsSync function completes" ); 
+
+
+function tossCoin() {
+    return Math.random() > 0.5 ? "heads" : "tails";
+  }
+  
+  function fiveHeads() {
+    return new Promise((resolve, reject) => {
+      let headsCount = 0;
+      let attempts = 0;
+      while (headsCount < 5) {
+        attempts++;
+        let result = tossCoin();
+        console.log(`${result} was flipped`);
+        if (result === "heads") {
+          headsCount++;
+        } else {
+          headsCount = 0;
+        }
+      }
+      if (attempts <= 100) {
+        resolve(`It took ${attempts} tries to flip five "heads"`);
+      } else {
+        reject("Attempts have exceeded 100 flips.");
+      }
+    });
+  }
+  fiveHeads()
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+  console.log("When does this run now?");
